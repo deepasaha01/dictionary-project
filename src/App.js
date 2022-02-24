3e3bfbd5005e93e9aebad7fea9586cd6a1c9e20f
deepasaha01 <90994589+deepasaha01@users.noreply.github.com>
2022-02-24 17:41:07 +0900

    Created search component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import Dictionary from "./Dictionary";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+      </div>
+      <footer className="App-footer">
+        <small>Coded by Deepa Saha</small>
+      </footer>
     </div>
   );
 }
