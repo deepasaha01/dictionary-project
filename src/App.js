66cd5eb75534fd029902842b9fff0b2594bae907
deepasaha01 <90994589+deepasaha01@users.noreply.github.com>
2022-03-03 10:59:34 +0900

    Updated styles

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <div className="container">
         <header className="App-header"></header>
         <main>
-          <Dictionary />
+          <Dictionary defaultKeyword="sunset" />
         </main>
       </div>
       <footer className="App-footer">
